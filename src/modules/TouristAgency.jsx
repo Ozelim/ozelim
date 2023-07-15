fa9868090004fb4fc321147fa0c8d77c6177617a
armanizone <kurama.zxc@mail.ru>
2023-07-15 18:44:18 +0600

    small changes adaptive

--- src/modules/TouristAgency.jsx
+++ src/modules/TouristAgency.jsx
@@ -1,7 +1,58 @@
+import { useMediaQuery } from '@mantine/hooks'
 import React from 'react'
 import { FcInfo } from 'react-icons/fc'
 
 export const TouristAgency = () => {
+
+  const matches = useMediaQuery(`(max-width: 878px)`)
+
+  if (matches) return (
+    <div className="w-full">
+      <div className="container">
+        <div className='flex flex-col'>
+          <h1 className="heading">
+            Откройте свое туристическое агентство «Поехали с нами»!
+          </h1>
+          <img
+            className="object-contain mt-8"
+            src="https://img.poehalisnami.kz/images/bg-open-office.png"
+            alt=""
+          />
+            <div className="flex max-w-sm mt-5">
+              <FcInfo className="text-5xl flex-shrink-0  mr-3" />
+              <div>
+                <h3 className="text-2xl text-[#2b3641]">
+                  Небольшие инвестиции
+                </h3>
+                <p className="mt-2">
+                  Для запуска собственного дела не понадобятся крупные
+                  финансовые вложения.
+                </p>
+              </div>
+            </div>
+            <div className="flex  max-w-sm mt-5 ">
+              <FcInfo className="text-5xl flex-shrink-0 mr-3" />
+              <div>
+                <h3 className="text-2xl  text-[#2b3641]">Успешный бизнес</h3>
+                <p className="mt-2">
+                  Вы присоединяетесь к динамично развивающейся крупной сети.
+                </p>
+              </div>
+            </div>
+            <div className="flex   max-w-sm mt-5">
+              <FcInfo className="text-5xl flex-shrink-0  mr-3" />
+              <div>
+                <h3 className="text-2xl  text-[#2b3641]">Легкий старт</h3>
+                <p className="mt-2">
+                  Для начала работы нужно ориентировочно 25–45 дней.
+                </p>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+
   return (
     <div className="w-full">
       <div className="container">
--- src/pages/resort/Resort.jsx
+++ src/pages/resort/Resort.jsx
@@ -9,7 +9,7 @@ export const Resort = () => {
         <div className='relative rounded-primary overflow-hidden space-y-2 pb-4'>
           <h1 className=" text-4xl mb-2">Lorem ipsum dolor sit.</h1>
           <p>Lorem, ipsum dolor.</p>
-          <div className='grid grid-cols-[60%_auto] border-gray-100 border-solid border-2 w-full rounded-primary'>
+          <div className='grid grid-cols-1 md:grid-cols-[60%_auto] border-gray-100 border-solid border-2 w-full rounded-primary'>
             <ResortSlider/>
             <ResortDetails/>
           </div>
