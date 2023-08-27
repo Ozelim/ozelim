e03135e2992bd757c47bef1604ff86660005dd9c
armanizone <kurama.zxc@mail.ru>
2023-08-27 23:34:59 +0600

    eshe

--- src/entities/newsCard/ui/Card.jsx
+++ src/entities/newsCard/ui/Card.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import { AiFillCalendar } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
-import { Avatar } from 'shared/ui'
-import { getImageUrl } from 'shared/lib'
-import dayjs from 'dayjs'
+import dayjs from "dayjs"
+import { AiFillCalendar } from "react-icons/ai"
+import { getImageUrl } from "shared/lib"
 
 export const Card = ({news}) => {
   return (
-    <div className="bg-white shadow rounded-primary max-w-5xl w-full mx-auto">
-      <div className="p-10 flex flex-col">
-        <div className="flex items-center">
-          <Link to={'/'}>
+    <div className="bg-white shadow rounded-primary max-w-4xl w-full mx-auto">
+      <div className="p-5 flex flex-col">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
             {getImageUrl(news, news?.avatar) ? (
               <img
                 className="w-14 h-14 bg-slate-300 rounded-full object-cover"
@@ -21,22 +18,25 @@ export const Card = ({news}) => {
             ) : (
               <div className="w-14 h-14 bg-slate-300 rounded-full object-cover" />
             )}
-          </Link>
-          <span className="ml-4 text-lg">{news?.name}</span>
+            <span className="ml-4 text-lg">{news?.name}</span>
+          </div>
+          <div className="flex">
+            <AiFillCalendar fill="teal" size={20} />
+            <span className="ml-1 text-slate-600 whitespace-nowrap">
+              {dayjs(news?.created).format('YY/MM/DD, HH:mm')}
+            </span>
+          </div>
         </div>
 
         <div className="flex justify-between">
           <h1 className="font-head font-bold my-4 text-4xl">
             {news?.title}
           </h1>
-          <div className="flex -mt-12">
-            <AiFillCalendar fill="teal" size={20} />
-            <span className="ml-1 text-slate-600 ">{dayjs(news?.date).format('DD/MM/YY, HH:mm')}</span>
-          </div>
+  
         </div>
         {getImageUrl(news, news?.image) ? (
           <img
-            className="w-full max-h-[400px] object-cover"
+            className="w-full  object-cover aspect-video"
             src={getImageUrl(news, news?.image)}
             loading="lazy"
             alt="travel"
--- src/widgets/BomjPlaza/ui/BomjPlaza.jsx
+++ src/widgets/BomjPlaza/ui/BomjPlaza.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { ActionIcon } from '@mantine/core'
 
 import { AiOutlineInstagram, AiOutlinePhone } from 'react-icons/ai'
 import { Button } from '@mantine/core'
 
 export const BomjPlaza = ({resort}) => {
+
+  console.log(resort?.whats);
+
   return (
     <div className="flex justify-between items-center max-w-2xl shadow-md border p-4 rounded-primary w-auto bg-white" >
       <div className='flex flex-col'>
@@ -12,16 +14,20 @@ export const BomjPlaza = ({resort}) => {
           {resort?.title} 
         </div>
         <p className="text-sm text">
-          Г. Шымкент склад в ашибулаке {resort?.adress}
+          {resort?.adress}
         </p>
       </div>
       <div className='flex gap-4 text-primary-500'>
-        <Button variant='outline' p={8} radius={9999}>
-          <AiOutlineInstagram className='text-2xl' />
-        </Button>
-        <Button variant='outline' p={8} radius={9999}>
-          <AiOutlinePhone className='text-2xl' />
-        </Button>
+        {resort?.inst && (
+          <Button variant='outline' p={8} radius={9999}>
+            <AiOutlineInstagram className='text-2xl' />
+          </Button>
+        )}
+        {resort?.whats && (
+          <Button variant='outline' p={8} radius={9999}>
+            <AiOutlinePhone className='text-2xl' />
+          </Button>
+        )}
       </div>
     </div>
   )
