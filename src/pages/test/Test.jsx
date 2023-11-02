3a7e3574b9f900628cebaeb93bd05d6d86f07a2a
armanizone <kurama.zxc@mail.ru>
2023-11-02 18:35:13 +0600

    save

--- src/pages/test/Test.jsx
+++ src/pages/test/Test.jsx
@@ -1,24 +1,5 @@
-import { BuyFranchise } from 'modules/BuyFranchise'
-import { CourseAbout } from 'modules/CourseAbout'
-import { CuratorCourse } from 'modules/CuratorCourse'
-import { HaveQuestions } from 'modules/HaveQuestions'
-import { JobTeachPractice } from 'modules/JobTeachPractice'
-import { NewFormat } from 'modules/NewFormat'
-import { OnlineCourseManager } from 'modules/OnlineCourseManager'
-import { OurPartners } from 'modules/OurPartners'
-import { PriceList } from 'modules/PriceList'
-import { Regions } from 'modules/Regions'
-import { Results } from 'modules/Results'
-import { TeachPacks } from 'modules/TeachPacks'
-import { TeachingProgram } from 'modules/TeachingProgram'
-import { TourWeb } from 'modules/TourWeb'
-import { TouristAgency } from 'modules/TouristAgency'
-import { VacationPlan } from 'modules/VacationPlan'
-import { Resort } from 'pages'
-import { TeachComfort } from 'pages/courses/ui/TeachComfort'
-import { WhyOurCourse } from 'pages/courses/ui/WhyOurCourse'
 import React from 'react'
-import { TourOperators } from 'modules/TourOperators'
+
 import { Button, TextInput } from '@mantine/core'
 import { pb } from 'shared/api'
 import axios from 'axios'
@@ -29,7 +10,38 @@ export const Test = () => {
 
   async function submit (e) {
     e.preventDefault()
-    await axios.post(`https://ecom.jysanbank.kz/ecom/api`)
+    const formData = new FormData()
+    formData.append('ORDER',1233211234)
+    formData.append('AMOUNT', 'KZT')
+    formData.append('CURRENCY',1233211234)
+    formData.append('MERCHANT','TEST_ECOM')
+    formData.append('DESC','ТЕСТ')
+    formData.append('LANGUAGE','ru')
+    formData.append('CLIENT_ID', 69)
+    formData.append('TERMINAL', 'WEB10004'),
+    
+    formData.append('NAME', 'EBALAI BABALAI')
+    
+    formData.append('crd_pan', '4413 2800 4692 5120')
+    formData.append('crd_exp', '04/23')
+    formData.append('crd_cvc', 618)
+
+    formData.append('NONCE', 'EBALAI 1698922631529')
+    formData.append('EMAIL', 'iartichshev@crystalspring.kz')
+    formData.append('BACKREF', 'https://www.google.kz/#q=crystalspring.kz')
+    formData.append('DESC_ORDER', `Какая то перечень херня какая то`)
+    formData.append('P_SIGN','ec15a559be256dba0194cc80f92ffaf5e76b72b451aba5ec904ef01c065b5d4df31001fb72b2560b690f2b8c23b2f3547ee00df1df439540f1d5542469088734')
+    
+    await axios.post(`https://ecom.jysanbank.kz/ecom/api`, formData
+      // SHARED_SECRET: '',
+      // ORDER: 1233211234,
+      // AMOUNT: 666,
+      // CURRENCY: 'KZT',
+      // MERCHANT: 'TEST_ECOM',
+      // TERMINAL: 'WEB10004',
+      // DESC: 'ТЕСТ',
+      // P_SIGN: `ec15a559be256dba0194cc80f92ffaf5e76b72b451aba5ec904ef01c065b5d4df31001fb72b2560b690f2b8c23b2f3547ee00df1df439540f1d5542469088734`
+    )
     .then(res => {
       console.log(res, 'res');
     })
