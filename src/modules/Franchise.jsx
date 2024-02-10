181344afa22110cb0c0e7bf57635b37c1c36038f
armanizone <kurama.zxc@mail.ru>
2024-02-10 23:59:15 +0600

    z

--- src/modules/Franchise.jsx
+++ src/modules/Franchise.jsx
@@ -3,26 +3,27 @@ import React from 'react'
 import { Link } from 'react-scroll'
 import { Link as RouterLink } from 'react-router-dom'
 import Kazmap from 'shared/assets/images/map-kz.png'
+import { useLangContext } from 'app/langContext'
 
 export const Franchise = () => {
+
+  const {kz} = useLangContext()
+
   return (
     <div className="w-full">
       <div className="container">
         <div className="flex lg:flex-row flex-col gap-4 justify-between items-center bg-gradient-to-tl from-teal-600 to-teal-500 shadow-md rounded-primary">
           <div className="ml-8">
-            <p className="text-4xl mt-2 text-white">Единый реестр санаторно-курортных комплексов Казахстана</p>
+            <p className="text-4xl mt-2 text-white">
+              {kz ? `Қазақстанның санаторияларының біріккен тізімі` : `Единый реестр санаторно-курортных комплексов Казахстана`}
+            </p>
             <p className="mt-5 text-2xl text-white ">
-              Поиск и подбор оздоровительных туров по Казахстану
+              {kz ? `Қазақстан бойынша сауықтыру турларын іздеу және іріктеу`: `Поиск и подбор оздоровительных туров по Казахстану`}
             </p>
             <div className="mt-5">
-              <Button component={RouterLink} to="/about" size="md">
-                Подробнее
+              <Button component={RouterLink} to="/resorts" size="md">
+                {kz ? `Толығырақ` : `Подробнее`}
               </Button>
-              <RouterLink to="/program">
-                <Button size="md" className="ml-3">
-                  Сотрудничество
-                </Button>
-              </RouterLink>
             </div>
           </div>
           <img
