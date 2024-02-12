67c423f4463b0ae1e337bd77fe8b3e3c51fc7a26
MIDstructure <midstructure@mail.ru>
2024-02-12 17:51:50 +0600

    team data fetching changed

--- src/pages/our-team/OurTeam.jsx
+++ src/pages/our-team/OurTeam.jsx
@@ -5,23 +5,6 @@ import { pb } from 'shared/api'
 import { usePageData } from 'shared/hooks'
 import { TeamCard } from 'shared/ui/TeamCard'
 
-async function getTeam() {
-  const text = await pb
-    .collection('text')
-    .getFullList({ filter: `page = 'team'` })
-  const images = await pb
-    .collection('images')
-    .getFullList({ filter: `page = 'team'` })
-  const slider = await pb
-    .collection('slider')
-    .getFullList({ filter: `page = 'team'` })
-  return {
-    text: text[0],
-    images: images[0],
-    slider: slider[0],
-  }
-}
-
 async function getOurTeam() {
   return await pb.collection('members').getFullList({filter: `kz = false`})
 }
@@ -33,18 +16,10 @@ async function getOurTeamKz() {
 export const OurTeam = () => {
 
   const [ourTeam, setOurTeam] = React.useState([])
-  const [team, setTeam] = React.useState({})
 
   const {images, text, headings} = usePageData('team')
 
   const {kz} = useLangContext()
-
-  React.useEffect(() => {
-    getTeam()
-    .then(res => {
-      setTeam(res)
-    })
-  }, [])
 
   React.useEffect(() => {
     if (kz) {
@@ -62,7 +37,6 @@ export const OurTeam = () => {
     <div className="w-full">
       <div className="container">
         <HealthHeader
-          health={team}
           images={images}
           headings={headings}
           text={text}
