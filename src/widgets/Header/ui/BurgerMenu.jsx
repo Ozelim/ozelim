16bce2cf1a9a4a0546fdcc62c89a8b97db011851
MIDstructure <midstructure@mail.ru>
2024-02-12 17:40:53 +0600

    a

--- src/widgets/Header/ui/BurgerMenu.jsx
+++ src/widgets/Header/ui/BurgerMenu.jsx
@@ -7,7 +7,7 @@ import { useLangContext } from 'app/langContext'
 const array = [
   { labelkz: `Жаңалықтар`, labelru: 'Новости', link: '/news' },
   { labelkz: `Біздің туристер`, labelru: 'Наши туристы', link: '/partners', disabled: true },
-  { labelkz: `Агенттік бағдарлама`, labelru: 'Партнерская программа', link: '/program' },
+  { labelkz: `Серiктестiк бағдарлама`, labelru: 'Партнерская программа', link: '/program' },
   { labelkz: `Біздің команда`, labelru: 'Наша команда', link: '/our-team', },
   { labelkz: `Мейірімділік`, labelru: 'Доброе дело', link: '/charity-fund', },
 ]
