7db39f9884d4fd99df7b978bd5f5b33540a99f3c
маверик <106444439+armanizone@users.noreply.github.com>
2024-04-22 18:30:41 +0500

    Update Profile.jsx

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -406,7 +406,7 @@ export const Profile = () => {
 
       const data = {
         ORDER: randomNumber,
-        AMOUNT: 5,
+        AMOUNT: 30000,
         // AMOUNT: 30000,
         CURRENCY: 'KZT',
         MERCHANT:'110-R-113431490',
