62be9ebbdc15e03bc4bf66ea32471650673876a4
armanizone <kurama.zxc@mail.ru>
2024-11-13 18:32:10 +0500

    check

---
+++ src/pages/market/market.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button, MantineProvider } from '@mantine/core'
+
+export const Market = () => {
+  return (
+      <Button>
+        Button
+      </Button>
+  )
+}
