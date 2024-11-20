555d5f3aef6ef4f8dc63c2b532bc1ecde85077d0
armanizone <kurama.zxc@mail.ru>
2024-11-21 00:57:48 +0500

    w

--- src/features/auth/forgotPassword/model/actions.js
+++ src/features/auth/forgotPassword/model/actions.js
@@ -2,6 +2,7 @@ import { pb } from 'shared/api'
 
 async function sendPasswordReset (user, email) {
   try {
+    pb.authStore.clear();
     return await pb.collection(user === 'user' ? 'users' : 'agents').requestPasswordReset(email)
   } catch (err) {
     throw err
@@ -10,6 +11,7 @@ async function sendPasswordReset (user, email) {
 
 async function resetPassword (user, token, password, passwordConfirm) {
   try {
+    pb.authStore.clear();
     return await pb.collection(user === 'user' ? 'users' : 'agents').confirmPasswordReset(token, password, passwordConfirm)
   } catch (err) {
     throw err
