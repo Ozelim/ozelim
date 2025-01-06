2956b25cf721a4511a0b31eee686b99c3bab4d4a
armanizone <kurama.zxc@mail.ru>
2025-01-06 18:39:13 +0500

    w

--- src/pages/tourist/Tourist.jsx
+++ src/pages/tourist/Tourist.jsx
@@ -707,14 +707,14 @@ export const Tourist = () => {
                 Агенский договор.pdf
               </a>
             </div>
-              {/* <div className='mt-2 flex gap-4'>
+              <div className='mt-2 flex gap-4'>
                 <Button
                   component={Link}
                   to={'/login?signup=true&agent=111924111111111'}
                 >
                   Ознакомлен и согласен
                 </Button>
-              </div> */}
+              </div>
             </div>
           </section>
 
